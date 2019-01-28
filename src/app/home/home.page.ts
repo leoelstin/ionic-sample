b4ecf5033f6c37b10ed5c57794c3c71a92ab7dd5
Leo Elstin <leo.elstin@azinova.info>
2019-01-28 14:57:21 +0530

    ** Committed By Leo Elstin **

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,17 +7,26 @@ import { JsonPipe } from '@angular/common';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  object : Object
-  printJson(noGuests: string, name: string, phone : String, mailId : String) {
    
-    var jsonData = {
-      'noGuests' : noGuests,
-      'name' : name,
-      'phone' : phone,
-      'mailId' : mailId
-    }
-    console.log(jsonData);
+   toggle1 = false
+   toggle2 = false
+   toggle3 = false 
 
-  }
+   updateItem () {
+     if(this.toggle1 && this.toggle2 && this.toggle3) {
+       // do the function
+     }
+   }
+  // object : Object
+  // printJson(noGuests: string, name: string, phone : String, mailId : String) {
+   
+  //   var jsonData = {
+  //     'noGuests' : noGuests,
+  //     'name' : name,
+  //     'phone' : phone,
+  //     'mailId' : mailId
+  //   }
+  //   console.log(jsonData);
+
+  // }
 }
